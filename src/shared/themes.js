d17194291d793d2a75d889e2cc0934ec4286c3d2
Rishav Bharti <rishavbharti21@gmail.com>
2022-01-23 00:12:34 +0530

    Setup theming

---
+++ src/shared/themes.js
@@ -0,0 +1,30 @@
+import { blue, yellow, red, green, neutral } from './colors';
+import { fontFamily } from './typography';
+
+export const defaultTheme = {
+  primary: blue[300],
+  primaryLight: blue[100],
+  primaryHover: blue[400],
+  primaryActive: blue[100],
+  formElementBackground: neutral[100],
+  textOnFormElementBackground: neutral[600],
+  textOnPrimary: neutral[100],
+  text: neutral[600],
+  textInverted: neutral[100],
+  primaryFont: fontFamily.primary,
+  disabled: neutral[400],
+  textOnDisabled: neutral[300],
+  textFieldBackground: neutral[200],
+  textFieldLabel: neutral[500],
+  status: {
+    warning: yellow[100],
+    warningHover: yellow[200],
+    warningActive: yellow[300],
+    error: red[100],
+    errorHover: red[200],
+    errorActive: red[300],
+    success: green[100],
+    successHover: green[200],
+    successActive: green[300],
+  },
+};
---
+++ src/shared/typography.js
@@ -0,0 +1,35 @@
+export const fontFamily = {
+  primary: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
+};
+
+export const fontWeight = {
+  regular: '400',
+  bold: '700',
+  extrabold: '800',
+  black: '900',
+};
+
+export const fontSize = {
+  xs: '12',
+  sm: '14',
+  base: '16',
+  md: '20',
+  lg: '24',
+  xl: '28',
+  '2xl': '32',
+  '3xl': '40',
+  '4xl': '48',
+  '5xl': '90',
+};
+
+export const typeScale = {
+  header1: '1.8rem',
+  header2: '1.6rem',
+  header3: '1.4rem',
+  header4: '1.2rem',
+  header5: '1.1rem',
+  paragraph: '1rem',
+  helperText: '0.8rem',
+  copyrightText: '0.7rem',
+};

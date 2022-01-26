b6d2585b17e03791e98b58c7db16e3a1366dc402
Rishav Bharti <rishavbharti21@gmail.com>
2022-01-26 15:48:05 +0530

    Create `global` styles

---
+++ src/shared/global.js
@@ -0,0 +1,23 @@
+import { css } from '@emotion/react';
+
+export const bodyStyles = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  div {
+    margin: 0;
+    padding: 0;
+  }
+`;

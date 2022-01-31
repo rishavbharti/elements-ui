7d7ac35c8f737bbd77f5fd0a91e584bab769cfa5
Rishav Bharti <rishavbharti21@gmail.com>
2022-01-31 14:23:35 +0530

    Add docs page

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
   stories: [
+    '../src/docs/**/*.stories.mdx',
     '../src/components/**/*.stories.mdx',
     '../src/components/**/*.stories.@(js|jsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
+  ],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
